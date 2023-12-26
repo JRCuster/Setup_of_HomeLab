55b51bcde805f29f312c2714bd58d8a19920cc9c
Joseph Custer <drrohin@roboticscorp.onmicrosoft.com>
2023-12-25 21:15:39 -0600

    Fixed django server with ChatGPT-4 onlinechat c 2 p from GPT-Pilot

--- AutoGen-Project/gpt-pilot/workspace/gpt-pilot/workspace/Setup_of_HomeLab/run_app.bat
+++ AutoGen-Project/gpt-pilot/workspace/gpt-pilot/workspace/Setup_of_HomeLab/run_app.bat
@@ -1,3 +1,5 @@
-@echo off
-cd /d C:\AutoGen-Project\gpt-pilot\workspace\gpt-pilot\workspace\Setup_of_HomeLab\homelab
+@echo off
+
+cd /d C:\AutoGen-Project\gpt-pilot\workspace\gpt-pilot\workspace\Setup_of_HomeLab\homelab
+
 python manage.py runserver 8080
--- homelab/homelab/settings.py
+++ homelab/homelab/settings.py
@@ -111,6 +111,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+ROOT_URLCONF = 'homelab.homelab.urls'
+WSGI_APPLICATION = 'homelab.homelab.wsgi.application'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
