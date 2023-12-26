cd3a5d044fbcf0811a79c4435cdfa7b2419894e6
Joseph Custer <drrohin@roboticscorp.onmicrosoft.com>
2023-12-25 22:23:44 -0600

    Got my homepage now

--- homelab/urls.py
+++ homelab/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views  # Make sure views is also correctly defined
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
