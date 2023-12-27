84eb038a1b22615df02da5dfdf7d2305f60f43ad
Joseph Custer <drrohinshadow@gmail.com>
2023-12-27 16:48:16 -0600

    Remove unnecessary code for real-time updates

--- homelab/static/js/dashboard.js
+++ homelab/static/js/dashboard.js
@@ -23,15 +23,10 @@ function updatePiHoleStatus() {
 
 // Call updatePiHoleStatus every 30 seconds
 setInterval(updatePiHoleStatus, 30000);
-            .then(response => response.json())
-            .then(data => {
-                // Update the dashboard with the fetched data
-                // Example: document.getElementById('cluster-status').textContent = data.cluster_status;
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }
-
-    // Fetch data every 15 seconds for real-time updates
-    setInterval(fetchData, 15000);
-});
-});
+fetch('/dashboard/data/')
+    .then(response => response.json())
+    .then(data => {
+        // Update the dashboard with the fetched data
+        // Example: document.getElementById('cluster-status').textContent = data.cluster_status;
+    })
+    .catch(error => console.error('Error fetching data:', error));
