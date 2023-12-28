812db316d9f026795f077516f5b8ad16e5e1aee8
Joseph Custer <drrohinshadow@gmail.com>
2023-12-28 16:42:30 -0600

    Update PostgreSQL image version and add new dependencies

--- homelab/urls.py
+++ homelab/urls.py
@@ -5,6 +5,7 @@ from homelab.views import pihole_status, dashboard
 # Rest of the code...
 
 urlpatterns = [
+    path('', dashboard, name='home'),
     path('admin/', admin.site.urls),
     path('pihole-status/', pihole_status, name='pihole_status'),
     path('dashboard/', dashboard, name='dashboard'),
